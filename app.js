0f8cdcab8c3b652ef34e2bc5fe15da86ff8554c3
JONATHAN JESUS DE BRITO <jonathan.jbrito@senacsp.edu.br>
2018-03-12 20:32:40 -0300

    First commit

---
+++ app.js
@@ -0,0 +1,9 @@
+var config = {
+    apiKey: "AIzaSyA3UxQ9KzeV7P4guxen-cNRtSWWeMYnyvw",
+    authDomain: "bd-bikcraft.firebaseapp.com",
+    databaseURL: "https://bd-bikcraft.firebaseio.com",
+    projectId: "bd-bikcraft",
+    storageBucket: "bd-bikcraft.appspot.com",
+    messagingSenderId: "69764807110"
+  };
+  firebase.initializeApp(config);
